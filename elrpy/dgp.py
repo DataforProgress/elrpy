20d4ca9af1ec7fb832d64b392ae52b200ac53c8b
njwfish <njwfish@gmail.com>
2024-07-09 14:50:05 -0700

    adding misspecified dgp options

--- elrpy/dgp.py
+++ elrpy/dgp.py
@@ -3,7 +3,7 @@ from jax import numpy as np
 from jax.nn import sigmoid
 
 from jax.experimental import sparse
-def normal_sim_binary(rng, n, d, k, beta=None, gamma=None, intercept=True, misspec=False, hetero_misspec=False):
+def normal_sim_binary(rng, n, d, k, beta=None, gamma=None, intercept=True, fixed_effects=False, random_effects=False):
     """Simulate data from a normal mixture model with binary outcomes.
 
     Args:
@@ -39,15 +39,15 @@ def normal_sim_binary(rng, n, d, k, beta=None, gamma=None, intercept=True, missp
     rng, next_rng = jax.random.split(rng)
     g = jax.random.categorical(rng, X @ gamma)
     G = sparse.BCOO((np.ones(g.shape[0]), np.vstack([np.arange(g.shape[0]), g]).T), shape=(g.shape[0], k)).T
-    # G = jax.nn.one_hot(g, k)
-    # G = sparse.BCOO.fromdense(G).T
 
     logits = (X @ beta)[:, None]
 
-    if misspec:
-        rng, next_rng = jax.random.split(rng)
-        alpha = G.T @ jax.random.normal(next_rng, shape=(k, 1))
-        if hetero_misspec:
+    if fixed_effects or random_effects:
+        alpha = np.zeros((n, 1))
+        if fixed_effects:
+            rng, next_rng = jax.random.split(rng)
+            alpha += G.T @ jax.random.normal(next_rng, shape=(k, 1))
+        if random_effects:
             rng, next_rng = jax.random.split(rng)
             z = jax.random.normal(next_rng, shape=(n, 1))
             alpha += z
@@ -56,13 +56,13 @@ def normal_sim_binary(rng, n, d, k, beta=None, gamma=None, intercept=True, missp
     rng, next_rng = jax.random.split(rng)
     Y = jax.random.bernoulli(next_rng, sigmoid(logits))
 
-    if misspec:
+    if fixed_effects or random_effects:
         return beta, gamma, X, Y, G @ Y, G, alpha
 
     return beta, gamma, X, Y, G @ Y, G
 
 
-def normal_sim_categorical(rng, n, d, k, p, eps=1e-6, beta=None, gamma=None):
+def normal_sim_categorical(rng, n, d, k, p, eps=1e-6, beta=None, gamma=None, intercept=True, fixed_effects=False, random_effects=False):
     """Simulate data from a normal mixture model with categorical outcomes.
     
     Args:
@@ -83,8 +83,11 @@ def normal_sim_categorical(rng, n, d, k, p, eps=1e-6, beta=None, gamma=None):
         dict: group outcome vectors
     """
     rng, next_rng = jax.random.split(rng)
-    X = jax.random.normal(next_rng, shape=(n, d - 1))
-    X = np.hstack([np.ones((n, 1)), X])
+    if intercept:
+        X = jax.random.normal(next_rng, shape=(n, d - 1))
+        X = np.hstack([np.ones((n, 1)), X])
+    else:
+        X = jax.random.normal(next_rng, shape=(n, d))
 
     if beta is None:
         rng, next_rng = jax.random.split(rng)
@@ -98,18 +101,25 @@ def normal_sim_categorical(rng, n, d, k, p, eps=1e-6, beta=None, gamma=None):
     rng, next_rng = jax.random.split(rng)
     g = jax.random.categorical(rng, X @ gamma)
     G = sparse.BCOO((np.ones(g.shape[0]), np.vstack([np.arange(g.shape[0]), g]).T), shape=(g.shape[0], k)).T
-    # G = jax.nn.one_hot(g, k)
-    # G = sparse.BCOO.fromdense(G).T
 
     logits = np.tensordot(X, beta, axes=1)
-    # if homo_misspec or hetero_misspec:
 
-    # if hetero_misspec:
-    #    rng, next_rng = jax.random.split(rng)
-    #    z = jax.random.normal(next_rng, shape=(n, 1))
-    #    beta_z = np.append(np.ones((1, p - 1)), 0)
-    #    logits += np.tensordot(z, beta_z, axes=1) 
+    if fixed_effects or random_effects:
+        alpha = np.zeros((n, p - 1))
+        if fixed_effects:
+            rng, next_rng = jax.random.split(rng)
+            alpha += G.T @ jax.random.normal(next_rng, shape=(k, p - 1))
+        if random_effects:
+            rng, next_rng = jax.random.split(rng)
+            z = jax.random.normal(next_rng, shape=(n, p - 1))
+            alpha += z
+        logits += alpha
+    
     rng, next_rng = jax.random.split(rng)
     Y = jax.random.categorical(rng, logits)
     Y = jax.nn.one_hot(Y, p)
+
+    if fixed_effects or random_effects:
+        return beta.flatten(), gamma, X, Y, G @ Y, G, alpha
+
     return beta.flatten(), gamma, X, Y, G @ Y, G
