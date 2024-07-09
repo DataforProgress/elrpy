1069ba41b430be032e236b649d8ee52741a80c3d
njwfish <njwfish@gmail.com>
2024-07-09 14:39:57 -0700

    adding misspecified dgp options

--- elrpy/dgp.py
+++ elrpy/dgp.py
@@ -3,7 +3,7 @@ from jax import numpy as np
 from jax.nn import sigmoid
 
 from jax.experimental import sparse
-def normal_sim_binary(rng, n, d, k, beta=None, gamma=None, intercept=True):
+def normal_sim_binary(rng, n, d, k, beta=None, gamma=None, intercept=True, misspec=False, hetero_misspec=False):
     """Simulate data from a normal mixture model with binary outcomes.
 
     Args:
@@ -30,7 +30,7 @@ def normal_sim_binary(rng, n, d, k, beta=None, gamma=None, intercept=True):
 
     if beta is None:
         rng, next_rng = jax.random.split(rng)
-        beta = jax.random.normal(next_rng, shape=(d ,1))
+        beta = jax.random.normal(next_rng, shape=(d,))
 
     if gamma is None:
         rng, next_rng = jax.random.split(rng)
@@ -39,10 +39,25 @@ def normal_sim_binary(rng, n, d, k, beta=None, gamma=None, intercept=True):
     rng, next_rng = jax.random.split(rng)
     g = jax.random.categorical(rng, X @ gamma)
     G = sparse.BCOO((np.ones(g.shape[0]), np.vstack([np.arange(g.shape[0]), g]).T), shape=(g.shape[0], k)).T
+    # G = jax.nn.one_hot(g, k)
+    # G = sparse.BCOO.fromdense(G).T
 
-    p = sigmoid(X @ beta)
+    logits = (X @ beta)[:, None]
+
+    if misspec:
+        rng, next_rng = jax.random.split(rng)
+        alpha = G.T @ jax.random.normal(next_rng, shape=(k, 1))
+        if hetero_misspec:
+            rng, next_rng = jax.random.split(rng)
+            z = jax.random.normal(next_rng, shape=(n, 1))
+            alpha += z
+        logits += alpha
+
     rng, next_rng = jax.random.split(rng)
-    Y = jax.random.bernoulli(next_rng, p)
+    Y = jax.random.bernoulli(next_rng, sigmoid(logits))
+
+    if misspec:
+        return beta, gamma, X, Y, G @ Y, G, alpha
 
     return beta, gamma, X, Y, G @ Y, G
 
@@ -82,10 +97,19 @@ def normal_sim_categorical(rng, n, d, k, p, eps=1e-6, beta=None, gamma=None):
 
     rng, next_rng = jax.random.split(rng)
     g = jax.random.categorical(rng, X @ gamma)
-    G = jax.nn.one_hot(g, k)
-    G = sparse.BCOO.fromdense(G).T
+    G = sparse.BCOO((np.ones(g.shape[0]), np.vstack([np.arange(g.shape[0]), g]).T), shape=(g.shape[0], k)).T
+    # G = jax.nn.one_hot(g, k)
+    # G = sparse.BCOO.fromdense(G).T
 
+    logits = np.tensordot(X, beta, axes=1)
+    # if homo_misspec or hetero_misspec:
+
+    # if hetero_misspec:
+    #    rng, next_rng = jax.random.split(rng)
+    #    z = jax.random.normal(next_rng, shape=(n, 1))
+    #    beta_z = np.append(np.ones((1, p - 1)), 0)
+    #    logits += np.tensordot(z, beta_z, axes=1) 
     rng, next_rng = jax.random.split(rng)
-    Y = jax.random.categorical(rng, np.tensordot(X, beta, axes=1))
+    Y = jax.random.categorical(rng, logits)
     Y = jax.nn.one_hot(Y, p)
-    return beta, gamma, X, Y, G @ Y, G
+    return beta.flatten(), gamma, X, Y, G @ Y, G
